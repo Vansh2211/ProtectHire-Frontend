65b42a1e0778e9281bfd67fd5c8f6fa7bf7010ba
Vansh Mehta <mehtavansh2211@gmail.com>
2025-07-15 08:03:39 +0000

    add this
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Menu, Search, UserPlus, Building2, LogIn, UserCircle, Settings, LogOut, Briefcase } from "lucide-react";
+import { Menu, Search, UserPlus, Building2, LogIn, Settings, LogOut, Briefcase } from "lucide-react";
 import { ProtectHireLogo } from "./ProtectHireLogo";
 import { useAuth } from "@/context/AuthContext";
 import {
@@ -163,7 +163,7 @@ export default function Header() {
                   <DropdownMenuSeparator />
                   <DropdownMenuItem asChild>
                     <Link href="/account">
-                      <UserCircle className="mr-2 h-4 w-4"/>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor" className="mr-2 h-4 w-4"><path d="M200-246q54-53 125.5-83.5T480-360q83 0 154.5 30.5T760-246v-514H200v514Zm280-194q58 0 99-41t41-99q0-58-41-99t-99-41q-58 0-99 41t-41 99q0 58 41 99t99 41ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm69-80h422q-44-39-99.5-59.5T480-280q-56 0-112.5 20.5T269-200Zm211-320q-25 0-42.5-17.5T420-580q0-25 17.5-42.5T480-640q25 0 42.5 17.5T540-580q0 25-17.5 42.5T480-520Zm0 17Z"/></svg>
                       My Account
                     </Link>
                   </DropdownMenuItem>
